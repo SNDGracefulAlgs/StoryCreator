86e5c9d105add5fb5022a38dbe88bf6e6f09a163
Nikita <Nikita@NIKITA-PC>
2018-07-19 01:04:38 +0300

    Node class implemented

---
+++ StoryCreator/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryCreator
+{
+    class Node
+    {
+        public string content;
+        public List<Heir> heirs;
+
+        public void AddHeir(string branchText, string content)
+        {
+            if (heirs == null) heirs = new List<Heir>();
+            heirs.Add(new Heir { branchText = branchText, heir = new Node { content = content, heirs = null } });
+        }
+    }
+
+    class Heir
+    {
+        public string branchText;
+        public Node heir;
+    }
+}
